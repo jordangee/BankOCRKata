205065a1c7aed1b0efc5a21459adff19b1c0a3a8
Matt Deckard <matt.deckard@asynchrony.com>
2018-03-12 13:42:51 -0500

    Initial Commit

---
+++ BankOCRKata/BankOCRKata.h
@@ -0,0 +1,19 @@
+//
+//  BankOCRKata.h
+//  BankOCRKata
+//
+//  Created by dev1 on 3/12/18.
+//  Copyright © 2018 Lilybug. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for BankOCRKata.
+FOUNDATION_EXPORT double BankOCRKataVersionNumber;
+
+//! Project version string for BankOCRKata.
+FOUNDATION_EXPORT const unsigned char BankOCRKataVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <BankOCRKata/PublicHeader.h>
+
+
